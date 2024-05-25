a4b5f31698268a22a854cfe5db3dcb3fe7543e14
Stanley Boateng <stankofb@gmail.com>
2024-05-25 19:32:48 +0000

    Changed background color

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>
+      <Text style={styles.text}>
         My name is <Text style={styles.nameText}>Stanley</Text>
       </Text>
     </View>
@@ -14,9 +14,12 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#fof8ff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontSize: 24,
   },
   nameText: {
     fontWeight: "bold",
